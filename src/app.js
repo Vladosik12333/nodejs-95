a85fbfba5ef86d00194f0df861b5343b73eb4974
Svitlana Humen <138813162+SvitlanaHu@users.noreply.github.com>
2024-04-14 15:52:45 +0300

    add route and clean code

--- src/app.js
+++ src/app.js
@@ -1,33 +1,27 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const { NODE_ENV } = require("./config");
+const route = require("./routes/products");
+
 const app = express();
-const { NODE_ENV } = require("./config")
 const formatLogger = NODE_ENV === "dev" ? "dev" : "short";
-const wrapper = require("./helpers/wrapper");
-const httpError = require("./helpers/httpError")
 
 app.use(morgan(formatLogger));
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/products", route)
 
-app.get("/", wrapper((req, res) => {
-  throw httpError(400, "my Error")
-  res.json({
-    status: "200",
-    message: "success",
-  });
-}));
 
-app.use((req, res) => {
-    res.status(404).json({
-        status: 404,
-        message: "Endpoint not found"
-    })
+app.use((_, res) => {
+  res.status(404).json({
+    status: 404,
+    message: "Endpoint not found"
+  })
 })
 
-app.use((err, req, res, next) => {
+app.use((err, _, res, __) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({
     status,
